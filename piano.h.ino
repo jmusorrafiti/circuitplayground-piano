07540d706f88f0be9f33da933d883a32993e5d0f
jmusorrafiti <33606911+jmusorrafiti@users.noreply.github.com>
2017-11-22 19:31:02 -0500

    Add files via upload
    new

--- piano.h.ino
+++ piano.h.ino
@@ -53,8 +53,8 @@ void silentNoteBlue(int p) {
   CircuitPlayground.setPixelColor(p, 0, 0, 255);
 }
 
-void playNote(int f, int t) {
-  CircuitPlayground.playTone(f, t, false);
+void playNote(int f) {
+  CircuitPlayground.playTone(f, 500, false);
 }
 
 
@@ -62,123 +62,130 @@ void loop() {
   //C3-B3
   //C  (Cap1 on)
   //if(cap0 < 300 && cap1 < 300 && cap2 < 300 && cap3 > 300){
-  if (cap1 > 75 && cap0 < 75 && cap3 < 75 && cap2 < 75) {
+  if (cap1 > 30 && cap0 < 30 && cap3 < 30 && cap2 < 30) {
     //CircuitPlayground.playTone(NOTE_C3,100, false);
     silentNoteRed(0);
-    playNote(NOTE_C3,100);
+    playNote(NOTE_C4);
   }
   //C#  (Cap0 on)
   //else  if(cap0 < 300 && cap1 < 300 && cap2 > 300 && cap3 < 300){
-  else if (cap0 > 75 && cap1 < 75 && cap2 < 75 && cap3 < 75) {
+  else if (cap0 > 30 && cap1 < 30 && cap2 < 30 && cap3 < 30) {
     //CircuitPlayground.playTone(NOTE_CS3,100,false);
     silentNoteYellow(0);
-    playNote(NOTE_CS3,100);
+    playNote(NOTE_CS4);
   }
   //D  (Cap 0 and 1 On)
-  else if (cap0 > 75 && cap1 > 75 && cap2 < 75 && cap3 < 75) {
+  else if (cap0 > 30 && cap1 > 30 && cap2 < 30 && cap3 < 30) {
     silentNoteRed(1);
-    playNote(NOTE_D3,100);
+    playNote(NOTE_D4);
   }
   //D# (cap2 on)
-  else if (cap2 > 75 && cap0 < 75 && cap1 < 75 && cap3 < 75) {
+  else if (cap2 > 30 && cap0 < 30 && cap1 < 30 && cap3 < 30) {
     silentNoteYellow(1);
-    playNote(NOTE_DS3,100);
+    playNote(NOTE_DS4);
   }
   //E  (cap 2 and 1 on)
-  else if (cap2 > 75 && cap1 > 75 && cap0 < 75 && cap3 < 75) {
+  else if (cap2 > 30 && cap1 > 30 && cap0 < 30 && cap3 < 30) {
     silentNoteRed(2);
-    playNote(NOTE_E3,100);
+    playNote(NOTE_E4);
   }
   //F  (cap 0 and 2 on)
-  else if (cap0 > 75 && cap2 > 75 && cap1 < 75 && cap3 < 75) {
+  else if (cap0 > 30 && cap2 > 30 && cap1 < 30 && cap3 < 30) {
     silentNoteRed(3);
-    playNote(NOTE_F3,100);
+    playNote(NOTE_F4);
   }
   //F# (cap 2 and 0 and 1 on)
-  else if (cap2 > 75 && cap0 > 75 && cap1 > 75 && cap3 < 75) {
+  else if (cap2 > 25 && cap0 > 25 && cap1 > 25 && cap3 < 30) {
     silentNoteYellow(2);
-    playNote(NOTE_FS3,100);
+    playNote(NOTE_FS4);
   }
   //G  (cap 3 on)
-  else if (cap3 > 75 && cap0 < 75 && cap1 < 75 && cap2 < 75) {
+  else if (cap3 > 30 && cap0 < 30 && cap1 < 30 && cap2 < 30) {
     silentNoteRed(4);
-    playNote(NOTE_G3,100);
+    playNote(NOTE_G4);
   }
   //G#  (cap3 and 1 on)
-  else if (cap3 > 75 && cap1 > 75 && cap0 < 75 && cap2 < 75) {
+  else if (cap3 > 30 && cap1 > 30 && cap0 < 30 && cap2 < 30) {
     silentNoteYellow(3);
-    playNote(NOTE_GS3,100);
+    playNote(NOTE_GS4);
   }
   //A   (cap 0 and 3 on)
-  else if (cap0 > 75 && cap3 > 75 && cap1 < 75 && cap2 < 75) {
+  else if (cap0 > 30 && cap3 > 30 && cap1 < 30 && cap2 < 30) {
     silentNoteBlue(0);
-    playNote(NOTE_A3,100);
+    playNote(NOTE_A4);
   }
   //A#  (cap3 and 0 and 1 on)
-  else if (cap3 > 75 && cap0 > 75 && cap1 > 75 && cap2 < 75) {
+  else if (cap3 > 25 && cap0 > 25 && cap1 > 25 && cap2 < 30) {
     silentNoteYellow(4);
-    playNote(NOTE_AS3,100);
+    playNote(NOTE_AS4);
   }
   //B  (cap 3 and 2 on)
-  else if (cap3 > 75 && cap2 > 75 && cap0 < 75 && cap1 < 75) {
+  else if (cap3 > 30 && cap2 > 30 && cap0 < 30 && cap1 < 30) {
     silentNoteBlue(1);
-    playNote(NOTE_B3,100);
+    playNote(NOTE_B4);
   }
 
   //C4-B4
   //C  (Cap12 on)
   //if(cap0 < 300 && cap1 < 300 && cap2 < 300 && cap3 > 300){
-  if (cap12 > 75 && cap10 < 75 && cap9 < 75 && cap6 < 75) {
+  if (cap12 > 30 && cap10 < 30 && cap9 < 30 && cap6 < 30) {
     //CircuitPlayground.playTone(NOTE_C3,100, false);
     silentNoteRed(5);
   }
   //C#  (Cap6 on)
   //else  if(cap0 < 300 && cap1 < 300 && cap2 > 300 && cap3 < 300){
-  else if (cap6 > 75 && cap10 < 75 && cap9 < 75 && cap12 < 75) {
+  else if (cap6 > 30 && cap10 < 30 && cap9 < 30 && cap12 < 30) {
     //CircuitPlayground.playTone(NOTE_CS3,100,false);
     silentNoteYellow(5);
   }
   //D  (Cap 6 and 12 On)
-  else if (cap6 > 75 && cap12 > 75 && cap10 < 75 && cap9 < 75) {
+  else if (cap6 > 30 && cap12 > 30 && cap10 < 30 && cap9 < 30) {
     silentNoteRed(6);
   }
   //D# (cap9 on)
-  else if (cap9 > 75 && cap10 < 75 && cap12 < 75 && cap6 < 75) {
+  else if (cap9 > 30 && cap10 < 30 && cap12 < 30 && cap6 < 30) {
     silentNoteYellow(6);
   }
   //E  (cap 9 and 12 on)
-  else if (cap9 > 75 && cap12 > 75 && cap6 < 75 && cap10 < 75) {
+  else if (cap9 > 30 && cap12 > 30 && cap6 < 30 && cap10 < 30) {
     silentNoteRed(7);
   }
   //F  (cap 9 and 6 on)
-  else if (cap9 > 75 && cap6 > 75 && cap10 < 75 && cap12 < 75) {
+  else if (cap9 > 30 && cap6 > 30 && cap10 < 30 && cap12 < 30) {
     silentNoteRed(8);
   }
   //F# (cap 9 and 6 and 12 on)
-  else if (cap9 > 75 && cap6 > 75 && cap12 > 75 && cap10 < 75) {
+  else if (cap9 > 30 && cap6 > 30 && cap12 > 30 && cap10 < 30) {
     silentNoteYellow(7);
   }
   //G  (cap 10 on)
-  else if (cap10 > 75 && cap9 < 75 && cap6 < 75 && cap12 < 75) {
+  else if (cap10 > 30 && cap9 < 30 && cap6 < 30 && cap12 < 30) {
     silentNoteRed(9);
   }
   //G#  (cap10 and 12 on)
-  else if (cap10 > 75 && cap12 > 75 && cap6 < 75 && cap9 < 75) {
+  else if (cap10 > 30 && cap12 > 30 && cap6 < 30 && cap9 < 30) {
     silentNoteYellow(8);
   }
   //A   (cap 10 and 6 on)
-  else if (cap10 > 75 && cap6 > 75 && cap12 < 75 && cap9 < 75) {
+  else if (cap10 > 30 && cap6 > 30 && cap12 < 30 && cap9 < 30) {
     silentNoteBlue(5);
   }
   //A#  (cap10 and 6 and 12 on)
-  else if (cap10 > 75 && cap6 > 75 && cap12 > 75 && cap9 < 75) {
+  else if (cap10 > 30 && cap6 > 30 && cap12 > 30 && cap9 < 30) {
     silentNoteYellow(9);
   }
   //B  (cap 10 and 9 on)
-  else if (cap10 > 75 && cap9 > 75 && cap12 < 75 && cap6 < 75) {
+  else if (cap10 > 30 && cap9 > 30 && cap12 < 30 && cap6 < 30) {
     silentNoteBlue(6);
   }
-
-
+  Serial.print("Cap 3:   ");
+  Serial.print(cap3);
+  Serial.print(" - Cap 2:  ");
+  Serial.print(cap2);
+  Serial.print(" - Cap 0:   ");
+  Serial.print(cap0);
+  Serial.print(" - Cap 1:  ");
+  Serial.println(cap1);
   CircuitPlayground.clearPixels();
+  delay(100);
 }
